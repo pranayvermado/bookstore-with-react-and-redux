60036a0124808948bf244d37879496601b024c6d
Pranay verma <pranayverma26@gmail.com>
2020-06-06 19:32:57 +0530

    missing file

---
+++ src/components/viewSelector.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class ViewSelector extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="btn-group btn-group-toggle" data-toggle="buttons">
+        <label
+          className="btn btn-primary btn-sm"
+          onClick={()=>this.props.onToggle("GRID")}
+        >
+          <input
+            type="radio"
+            name="options"
+            id="option1"
+            autoComplete="off"
+          ></input>
+          Grid View
+        </label>
+        <label
+          className="btn btn-primary btn-sm"
+          onClick={()=>this.props.onToggle("TABLE")}
+        >
+          <input
+            type="radio"
+            name="options"
+            id="option2"
+            autoComplete="off"
+          ></input>
+          Table View
+        </label>
+      </div>
+    );
+  }
+}
+
+export default ViewSelector;
